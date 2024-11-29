daf96090ae628f4bff0a0bb8ce8393f72f357400
Sérgio Raphael A. Sampaio <67436514+rapharage@users.noreply.github.com>
2024-11-29 03:13:09 -0300

    Update app.py

--- app.py
+++ app.py
@@ -29,4 +29,4 @@ def analyze_emotion():
 # Run the app and bind it to the correct port
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))  # Use Render's port or fallback to 5000
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port)
